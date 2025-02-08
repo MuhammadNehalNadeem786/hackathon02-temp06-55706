5597e9f5ffb10fc51cf0e4b7bddf6006dd276d0c
Muhammad Nehal Nadeem <muhammadnehalnadeem@gmail.com>
2025-02-09 00:32:05 +0500

    Update Authentication Code

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -18,7 +18,7 @@ const Login = () => {
 
                                 <h1 className='text-lg font-medium text-center mt-4'>Congratulation You Are Sign in</h1>
                                 <h2 className='text-lg font-bold text-center mt-4'>WELCOME to Furniro</h2>
-                                <p className='text-base text-center mt-4'>Furniture as Unique as You Are</p>
+                                <p className='text-base text-center mt-4'>Discover. Design. Delight. 🏡 Create Your Dream Home Shop Now! 🛒</p>
                             </div>
                         </SignedIn>
                     </div>
@@ -59,15 +59,10 @@ const Login = () => {
                         </form>
 
                         {/* Login Button */}
-                        <Link href={`https://solid-bunny-40.accounts.dev/sign-in?redirect_url=http%3A%2F%2Flocalhost%3A3000%2Flogin`}>
 
-                            <div className=' text-center w-full bg-red-500 text-white py-3 rounded-lg hover:bg-red-600 cursor-pointer'>
-
-                                <SignInButton />
-
-
-                            </div>
-                        </Link>
+                        <div className=' text-center w-full bg-red-500 text-white py-3 rounded-lg hover:bg-red-600 cursor-pointer'>
+                            <SignInButton />
+                        </div>
 
                         <p className="text-center mt-4 text-gray-600">
                             No account? <Link href="#" className="text-red-500 font-bold hover:underline">Create an account</Link>
